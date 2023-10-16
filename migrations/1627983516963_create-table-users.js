4fe3b6a83b51b12b421f232fb61ce71faf814c0e
masdikaid <masdikaid@gmail.com>
2023-10-16 21:53:21 +0700

    Initial commit

---
+++ migrations/1627983516963_create-table-users.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+        type: 'TIMESTAMP',
+        notNull: true,
+        default: pgm.func('NOW()')
+    }
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
